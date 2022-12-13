1b5b267deae99efe5aae3a7d5acc44cc028570f5
Gabriel Gamboa <gabriel.gamboa@mblabs.com.br>
2022-12-12 21:22:32 -0300

    feat: initial commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { Header } from './components/Header';
+import { Post } from './Post';
+
+import './style.css';
+
+export function App() {
+  return (
+    <div>
+      <Header />
+
+      <Post
+        author="Gabriel Gambôa"
+        content="aaaaaaaaaaaaaaa"
+      />
+
+      <Post
+        author="Gabriel Gambôa"
+        content="Um novo postzada"
+      />
+    </div>
+  );
+}
+
