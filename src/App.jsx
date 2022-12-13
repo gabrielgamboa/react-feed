61254d602832fb4ea5f8baea2ac55a9359cee7bc
Gabriel Gambôa <gabriel_gamboa10@hotmail.com>
2022-12-13 16:20:50 -0300

    feat: initial sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,30 @@
 import { Header } from './components/Header';
 import { Post } from './Post';
 
+import styles from './App.module.css';
+
 import './global.css';
+import { Sidebar } from './components/Sidebar';
 
 export function App() {
   return (
     <div>
       <Header />
 
-      <Post
-        author="Gabriel Gambôa"
-        content="aaaaaaaaaaaaaaa"
-      />
+      <div className={styles.wrapper}>
+        <Sidebar/>
+        <main>
+          <Post
+          author="Gabriel Gambôa"
+          content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Et ducimus, voluptatem debitis ipsum cupiditate beatae, harum quia rerum laboriosam voluptatum cumque deleniti, placeat illo perspiciatis voluptas inventore iusto odit voluptates!"
+         />
 
-      <Post
-        author="Gabriel Gambôa"
-        content="Um novo postzada"
-      />
+          <Post
+            author="Gabriel Gambôa"
+            content="Um novo postzada"
+         />
+        </main>
+      </div>
     </div>
   );
 }
