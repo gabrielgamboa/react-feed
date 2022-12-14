58b38534f89482b829ecdb8615777be0021dd4ae
Gabriel Gambôa <gabriel_gamboa10@hotmail.com>
2022-12-14 09:49:48 -0300

    feat: avatar css and pencil icon in sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,4 @@
+import { PencilLine } from 'phosphor-react'
 import styles from './Sidebar.module.css';
 
 export function Sidebar() {
@@ -6,12 +7,14 @@ export function Sidebar() {
             <img className={styles.cover} src="https://images.unsplash.com/photo-1578632749014-ca77efd052eb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=80" alt="" />
 
             <div className={styles.profile}>
+                <img className={styles.avatar} src="https://github.com/gabrielgamboa.png"/>
                 <strong>Gabriel Gambôa</strong>
                 <span>Web Developer</span>
             </div>
 
             <footer>
                 <a href="#">
+                    <PencilLine/>
                     Editar seu perfil
                 </a>
             </footer>
