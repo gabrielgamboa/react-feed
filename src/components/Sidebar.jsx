7adad5c283a032fb597bbcdc042ee88f7f89e5a6
Gabriel Gambôa <gabriel_gamboa10@hotmail.com>
2022-12-16 09:14:14 -0300

    feat: finish sidebar component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -14,7 +14,7 @@ export function Sidebar() {
 
             <footer>
                 <a href="#">
-                    <PencilLine/>
+                    <PencilLine size={20}/>
                     Editar seu perfil
                 </a>
             </footer>
