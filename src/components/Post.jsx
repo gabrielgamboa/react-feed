491ea8fb1bde50051a20174e52bf6cc81b98e82f
Gabriel Gamboa <gabriel.gamboa@mblabs.com.br>
2022-12-16 21:03:42 -0300

    feat: create comment form structure

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -18,12 +18,20 @@ export function Post() {
                 <p>Fala galeraa 👋</p>
                 <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
                 <p>👉 <a href=""> jane.design/doctorcare</a> </p>
-                <p> 
+                <p>
                     <a href="">#novoprojeto</a>{' '}
                     <a href="">#nlw</a>{' '}
                     <a href="">#rocketseat</a>{' '}
                 </p>
             </div>
+
+            <form className={styles.commentForm}>
+                <strong>Deixe seu feedback</strong>
+                <textarea
+                    placeholder="Deixe um comentário"
+                />
+                <button type="submit">Comentar</button>
+            </form>
         </article>
     );
 }
