a0687cb66a177ba95c094365ddbeb85ef19a63cb
Gabriel Gamboa <gabriel.gamboa@mblabs.com.br>
2023-01-16 20:01:59 -0300

    feat: use key to render lists

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -15,7 +15,7 @@ export function Comment({ content }) {
                             <time title="11 de maio às 08:14h" dateTime="2022-05-11 08:13:30">Cerca de 1h atrás</time>
                         </div>
 
-                        <button title="Deletar comentário">
+                        <button onClick={handleDeleteComment} title="Deletar comentário">
                             <Trash size={24}/>
                         </button>
                     </header>
